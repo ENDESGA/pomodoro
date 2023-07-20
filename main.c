ccc8dbe3456b7d63927046d01f896da8d28447e9
ENDESGA <38831818+ENDESGA@users.noreply.github.com>
2023-07-20 17:21:00 +1200

    ///////

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 #ifdef _WIN32
 	#define OS 0
@@ -11,21 +10,14 @@
 	#define C() system( "clear" )
 #endif
 
-void S( clock_t t )
-	{
-		clock_t end_time;
-		end_time = clock() + t * CLOCKS_PER_SEC;
-		while( clock() < end_time ) {}
-	};
-
 #define P printf
 char* e;
 char cc[ 50 ];
-#define T( c, t )                                                     \
-	for( i = c; i--; )                                                  \
-		S( 1 ), g &&P( "\r%02d:%02d", i / 60, i % 60 ), fflush( stdout ); \
-	P( "\n" );                                                          \
-	sprintf( cc, OS ? "./%s %d" : "start %s %d", e, t );                \
+#define T( c, t )                                                                         \
+	for( i = c; i--; )                                                                      \
+		system( "timeout 1 >nul" ), g &&P( "\r%02d:%02d", i / 60, i % 60 ), fflush( stdout ); \
+	P( "\n" );                                                                              \
+	sprintf( cc, OS ? "./%s %d" : "start %s %d", e, t );                                    \
 	system( cc )
 
 int main( int _a, char* _s[] )
